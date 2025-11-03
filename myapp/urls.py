62c928281b1df7c4a4b4b8fc7d27ca8437e54e49
Abinsanto <santoabin391@gmail.com>
2025-11-03 22:15:56 +0530

    Updated templates, CSS styles, and added new features

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,9 +15,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login_page, name='login_page'),
     path('signup/', views.signup_page, name='signup_page'),
-    path('login/', views.login_page, name='login_page'),
+    path('home/', views.home, name='home'),
     path('logout/', views.logout_user, name='logout_user'),
     path('delete/<int:id>/', views.delete_book, name='delete_book'),
 ]
