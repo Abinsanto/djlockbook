b90d8344aa7ff6eab43269b2b273313eb62b52b3
Abinsanto <santoabin391@gmail.com>
2025-11-05 20:53:01 +0530

    Fix ALLOWED_HOSTS syntax

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3guca7$=jhyp=pl^g4-c4zfj)_np*o71z80!#u5s=)odi#@mi6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = *
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
